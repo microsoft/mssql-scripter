5ad19cdabbe05ec0523c8e83e3c4841a8aa142e2
Brian O'Neill <pensivebrian@hotmail.com>
2017-03-31 14:08:36 -0700

    Accumulate code coverage data

--- scripts/run_all_tests.bat
+++ scripts/run_all_tests.bat
@@ -5,8 +5,9 @@ REM Copyright (c) Microsoft Corporation. All rights reserved.
 REM Licensed under the MIT License. See License.txt in the project root for license information.
 REM --------------------------------------------------------------------------------------------
 
-coverage run --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/common/tests
-coverage run --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/contracts/tests
-coverage run --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/tests
+coverage erase
+coverage run -a --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/common/tests
+coverage run -a --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/contracts/tests
+coverage run -a --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/tests
 
 coverage report -m
