4e284fb10003678043289219af9da4f3e2cccec9
Ron Quan <ronquan@microsoft.com>
2017-03-31 12:18:10 -0700

    Readding lowercased run all test bat file

---
+++ scripts/run_all_tests.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM --------------------------------------------------------------------------------------------
+REM Copyright (c) Microsoft Corporation. All rights reserved.
+REM Licensed under the MIT License. See License.txt in the project root for license information.
+REM --------------------------------------------------------------------------------------------
+
+coverage run --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/common/tests
+coverage run --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/contracts/tests
+coverage run --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/tests
+
+coverage report -m
