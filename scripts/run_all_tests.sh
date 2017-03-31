8927539a5feac8cac83947c30af8f1050f602f64
Brian O'Neill <broneill@microsoft.com>
2017-03-31 14:12:30 -0700

    Accumulate code coverage data (#9)

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -4,8 +4,9 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-coverage run --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/common/tests
-coverage run --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/contracts/tests
-coverage run --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/tests
+coverage erase
+coverage run -a --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/common/tests
+coverage run -a --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/contracts/tests
+coverage run -a --concurrency=thread -m unittest discover -s ../mssql-scripter/mssql/tests
 
 coverage report -m
