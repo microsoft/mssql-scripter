c0d294d4df515ecfb060fc6df0d1b929d671d6c8
Ron Quan <ronquan@microsoft.com>
2017-04-14 14:40:20 -0700

    Fixing test issue during testing

--- mssqlscripter/tests/test_parser.py
+++ mssqlscripter/tests/test_parser.py
@@ -39,7 +39,7 @@ class TestParser(unittest.TestCase):
         self.assertEqual(parameters.ConnectionString, 'Server=TestServer;Database=mydatabase;User Id=my_username;Password=secret;')
 
         standard_connection = [u'-S', u'TestServer', u'-d', u'mydatabase', u'-U', 'my_username']
-        #os.environ[parser.MSSQL_SCRIPTER_PASSWORD] = u'secret123ABC'
+        os.environ[parser.MSSQL_SCRIPTER_PASSWORD] = u'secret123ABC'
         parameters = parser.parse_arguments(standard_connection)
         self.assertEqual(parameters.ConnectionString, u'Server=TestServer;Database=mydatabase;User Id=my_username;Password=secret123ABC;')
 
