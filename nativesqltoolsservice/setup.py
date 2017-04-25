44f8d77d0ef6447766f89b5cc70fb4769e409874
ron <ronquan@microsoft.com>
2017-04-25 11:21:17 -0400

    Adding support to build native sql tools service wheels for each supported platform

---
+++ nativesqltoolsservice/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from setuptools import setup
+import os
+import sys
+
+VERSION = "0.1.1.dev0"
+# Find the platform we are building against.
+# This file should not be called directly.
+PLATFORM = os.environ['SQLTOOLSSERVICE_CURRENT_PLATFORM']
+
+DEPENDENCIES = [
+    'wheel'
+]
+
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License',
+]
+
+
+setup(
+    install_requires=DEPENDENCIES,
+    name='sqltoolsservice_{0}'.format(PLATFORM),
+    version=VERSION,
+    description='Microsoft SQL Tools service',
+    license='MIT',
+    author='Microsoft Corporation',
+    author_email='sqlxplatclieng@microsoft.com',
+    url='https://github.com/Microsoft/sql-xplat-cli/',
+    zip_safe=True,
+    classifiers=CLASSIFIERS,
+    include_package_data=True,
+    packages=[
+        'sqltoolsservice'
+    ],
+)
---
+++ nativesqltoolsservice/sqltoolsservice/__init__.py
@@ -0,0 +1,7 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# Template package that stores native sql tools service binaries during wheel compilation.
+# Files will be dynamically created here and cleaned up after each run.
