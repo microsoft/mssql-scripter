935f08e8dfff71a8dfda237bb0cc4fa97cab1cd6
Ronald Quan <rquan07@gmail.com>
2017-04-27 14:54:08 -0400

    Public preview/wheel dependency (#48)
    * Adding support to build native sql tools service wheels for each supported platform
    
    * Updating gitignore with path to downloaded binaries
    
    * Adding license + readme
    
    * Fixing extra tab and removing unused parameter in setu
    
    * Renamed buildallwheels
    
    * Addressed comments, removed native keyword and fixed hyphen in scripter name.
    
    * Renamed package and top level directory to mssqltoolsservice. Fixed bug in setup.py and readded flag to include package data
    
    * Updating gitignore
    
    * Adding helper function in mssqltoolsservice package so it can locate it's program relative to itself.
    
    * Changed name of environment variable for sqltoolservice platform. Added verbose message during building of the wheel.
    
    * Adding wheel as a requirement
    
    * Removed wheel dependency from mssqltoolsservice
    
    * Updating github url for mssqltoolsservice package
    
    * Adding build directory clean up along with cleaning both build and target directories prior to each run.
    
    * Update error message if mssqltoolsservice program is not found

---
+++ mssqltoolsservice/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from setuptools import setup
+import os
+import sys
+
+# Version must be in sync with mssqlscripter.
+VERSION = "0.1.1.dev0"
+# Find the platform we are building against.
+# This file should not be called directly.
+PLATFORM = os.environ['MSSQLTOOLSSERVICE_PLATFORM']
+
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License',
+]
+
+setup(
+    name='mssqltoolsservice_{}'.format(PLATFORM),
+    version=VERSION,
+    description='Microsoft SQL Tools service',
+    license='MIT',
+    author='Microsoft Corporation',
+    author_email='sqlxplatclieng@microsoft.com',
+    url='https://github.com/Microsoft/sqltoolsservice',
+    zip_safe=True,
+    classifiers=CLASSIFIERS,
+    include_package_data=True,
+    packages=[
+        'mssqltoolsservice'
+    ],
+)
