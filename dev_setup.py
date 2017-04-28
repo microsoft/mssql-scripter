74e40cae7c53bbe3f333fa4125a94c511ad9dd4f
Ron Quan <ronquan@microsoft.com>
2017-04-28 15:19:15 -0700

    Updating dev setup to use utility module

--- dev_setup.py
+++ dev_setup.py
@@ -4,33 +4,24 @@
 # --------------------------------------------------------------------------------------------
 
 from __future__ import print_function
-from subprocess import check_call, CalledProcessError
 
 import os
 import setup
 import sys
+import utility
 
 root_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
-
-def exec_command(command):
-    try:
-        print('Executing: ' + command)
-        check_call(command.split(), cwd=root_dir)
-        print()
-    except CalledProcessError as err:
-        print(err, file=sys.stderr)
-        sys.exit(1)
 
 print('Running dev setup...')
 print('Root directory \'{}\'\n'.format(root_dir))
 
 # install general requirements.
-exec_command('pip install -r requirements.txt')
+utility.exec_command('pip install -r requirements.txt', root_dir)
 
 # install mssqltoolsservice if this platform supports it.
 mssqltoolsservice_package_name = os.environ['MSSQLTOOLSSERVICE_PACKAGE_NAME']
 print('Installing {}...'.format(mssqltoolsservice_package_name))
 # mssqltoolsservice package name is retrieved from environment variable set by setup.py.
-exec_command('pip install {}'.format(mssqltoolsservice_package_name))
+utility.exec_command('pip install {}'.format(mssqltoolsservice_package_name), root_dir)
 
 print('Finished dev setup.')
