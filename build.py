7fe7416be67f2688aa65666cafbde9de7f646c01
Ronald Quan <rquan07@gmail.com>
2017-11-10 13:13:58 -0800

    Ron/platform wheels support (#161)
    * Merge release 1.0.0a21 (#155)
    
    * Use SqlToolsService built on .NET Core 2.0 and a build script updates (#131)
    
    * Bump version to 1.0.0a19
    
    * Use .NET Core 2.0 RTM built sqltoolsservice
    
    * Add build script to upload to azure blob storage
    
    * Upgrade to VS 2017
    
    * Remove 3.3 as supported Python version
    
    * Fix perf issue where main event loop takes 100% of CPU (#132)
    
    Fix perf issue where main event loop takes 100% of CPU
    
    We have a 2 threads:
    Thread #1 runs in a loop polling the response queue
    Thread #2 runs in a loop decoding responses from the sqltoolsservice over stdout and posting them to the response queue
    
    Since thread #1 doesn't sleep, it's takes 100% CPU. In addition, running python 2.7 on windows, #2 doesn’t preempt the CPU due to #1 taking all of the CPU cycles, so no response is processed.
    
    Fix is simple – thread #1 needs to sleep so thread #2 can get scheduled and get it’s work done.
    
    * Refine event loop perf fix in main.py
    
    Refine event loop perf fix in main.py
    
    * Fixing regular expression
    
    Previous regex would result in release:a1 and release_version: 12.
    Modified the regex for part Release to only pick up lower case letters.
    
    * Adding missing forward slash on test pypi url
    
    * fixing typos/grammar (#138)
    
    fixing typos/grammar.
    
    * Updating to release version 1.0.0a20.
    
    * Create doc for official msft docs page
    
    * Updated documentation page with usage_guide
    
    * Added link to download adventureworks
    
    * Updated with sqlcmd usage
    
    * Added in run and cloud shell support
    
    * Update/consolidate linux install (#153)
    
    * universal linux wheel gen and setup update.
    
    * Updating version cfg.
    
    * Updating sqltoolsservice container.
    
    * Updating spacing for flake8.
    
    * Updating team email. (#154)
    
    * Updating mssqltoolsservice to be integrated as a package of mssqlscripter.
    
    * Updating sqltoolsservice to be loaded from the repro instead of storage account.
    
    * Fix index file generation for daily storage account.
    
    * Fixing manylinux1 tag.
    
    * Updating platform tag for win x64.
    
    * Renaming sqltoolsservice win x64 folder.
    
    * Adding platform tags for win_amd64, manylinux1_x86_64, manylinux1_i686.
    
    * version bumping to 1.0.0a22.
    
    * Flake8 format fixes.
    
    * Erroring out when build receives invalid flag.

--- build.py
+++ build.py
@@ -6,23 +6,53 @@
 # --------------------------------------------------------------------------------------------
 
 from __future__ import print_function
-import os
-import re
-import sys
-import tempfile
-import utility
 from azure.storage.blob import BlockBlobService, ContentSettings
+import os
+import sys
+import utility
+import mssqlscripter.mssqltoolsservice.external as mssqltoolsservice
 
 AZURE_STORAGE_CONNECTION_STRING = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
 BLOB_CONTAINER_NAME = 'simple'
-UPLOADED_PACKAGE_LINKS = [] 
+UPLOADED_PACKAGE_LINKS = []
 
 
 def print_heading(heading, f=None):
     print('{0}\n{1}\n{0}'.format('=' * len(heading), heading), file=f)
 
 
-def upload_index_file(service, blob_name, title, links):
+def build(platform_names):
+    """
+        Builds mssql-scripter package.
+    """
+    print_heading('Cleanup')
+
+    # clean
+    utility.clean_up(utility.MSSQLSCRIPTER_DIST_DIRECTORY)
+    utility.cleaun_up_egg_info_sub_directories(utility.ROOT_DIR)
+
+    print_heading('Running setup')
+
+    # install general requirements.
+    utility.exec_command('pip install -r dev_requirements.txt', utility.ROOT_DIR)
+
+    # convert windows line endings to unix for mssql-cli bash script
+    utility.exec_command('python dos2unix.py mssql-scripter mssql-scripter', utility.ROOT_DIR)
+
+    for platform in platform_names:
+        mssqltoolsservice.copy_sqltoolsservice(platform)
+
+        print_heading('Building mssql-scripter {} wheel package package'.format(platform))
+        utility.exec_command('python --version', utility.ROOT_DIR)
+        utility.exec_command(
+            'python setup.py check -r -s bdist_wheel --plat-name {}'.format(platform), 
+            utility.ROOT_DIR,
+            continue_on_error=False)
+        
+        mssqltoolsservice.clean_up_sqltoolsservice()
+
+
+def _upload_index_file(service, blob_name, title, links):
     print('Uploading index file {}'.format(blob_name))
     service.create_blob_from_text(
         container_name=BLOB_CONTAINER_NAME,
@@ -37,11 +67,11 @@ def upload_index_file(service, blob_name, title, links):
             content_language=None,
             content_md5=None,
             cache_control=None
-        )
+            )
     )
 
 
-def gen_pkg_index_html(service, pkg_name):
+def _gen_pkg_index_html(service, pkg_name):
     links = []
     index_file_name = pkg_name+'/'
     for blob in list(service.list_blobs(BLOB_CONTAINER_NAME, prefix=index_file_name)):
@@ -49,11 +79,11 @@ def gen_pkg_index_html(service, pkg_name):
             # Exclude the index file from being added to the list
             continue
         links.append(blob.name.replace(index_file_name, ''))
-    upload_index_file(service, index_file_name, 'Links for {}'.format(pkg_name), links)
+    _upload_index_file(service, index_file_name, 'Links for {}'.format(pkg_name), links)
     UPLOADED_PACKAGE_LINKS.append(index_file_name)
 
 
-def upload_package(service, file_path, pkg_name):
+def _upload_package(service, file_path, pkg_name):
     print('Uploading {}'.format(file_path))
     file_name = os.path.basename(file_path)
     blob_name = '{}/{}'.format(pkg_name, file_name)
@@ -62,63 +92,84 @@ def upload_package(service, file_path, pkg_name):
         blob_name=blob_name,
         file_path=file_path
     )
-    gen_pkg_index_html(service, pkg_name)
 
 
-def build(options):
+def validate_package(platform_names):
+    """
+        Install mssql-scripter wheel package locally.
+    """
+    root_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
+    # Local install of mssql-scripter.
+    mssqlscripter_wheel_dir = os.listdir(utility.MSSQLSCRIPTER_DIST_DIRECTORY)
+    current_platform = utility.get_current_platform()
 
-    supported_actions = ['nightly']
-    action = None
+    mssqlscripter_wheel_name = [pkge for pkge in mssqlscripter_wheel_dir if current_platform in pkge]
 
-    if len(options) >= 1:
-        if options[0] not in supported_actions:
-            print('Please provide a supported action {}.'.format(supported_actions))
-            return
-        action = options[0]
-
-    if action == 'nightly':
-        assert AZURE_STORAGE_CONNECTION_STRING, 'Set AZURE_STORAGE_CONNECTION_STRING environment variable'
-
-    print_heading('Cleanup')
-
-    # clean
-    utility.clean_up(utility.MSSQLSCRIPTER_DIST_DIRECTORY)
-    utility.clean_up(utility.MSSQLTOOLSSERVICE_DIST_DIRECTORY)
-    utility.cleaun_up_egg_info_sub_directories(utility.ROOT_DIR)
-    utility.cleaun_up_egg_info_sub_directories(utility.MSSQLTOOLSSERVICE_DIRECTORY)
+    # To ensure we have a clean install, we disable the cache as to prevent cache overshadowing actual changes made.
+    utility.exec_command(
+       'pip install --no-cache-dir --no-index ./dist/{}'.format(mssqlscripter_wheel_name),
+       root_dir, continue_on_error=False)
     
-    print_heading('Running setup')
-    
-    # install general requirements.
-    utility.exec_command('pip install -r dev_requirements.txt', utility.ROOT_DIR)
-    
-    print_heading('Running mssql-scripter tests')
-    utility.exec_command('tox', utility.ROOT_DIR, continue_on_error = False)
-    
-    print_heading('Building mssql-scripter pip package')
-    utility.exec_command('python setup.py check -r -s sdist', utility.ROOT_DIR, continue_on_error = False)
-    
-    print_heading('Building mssqltoolsservice pip package')
-    utility.exec_command('python buildwheels.py', utility.MSSQLTOOLSSERVICE_DIRECTORY, continue_on_error = False)
 
-    if action == 'nightly':
-        blob_service = BlockBlobService(connection_string=AZURE_STORAGE_CONNECTION_STRING)
-    
-        print_heading('Uploading packages to blob storage ')
-        for pkg in os.listdir(utility.MSSQLSCRIPTER_DIST_DIRECTORY):
-            pkg_path = os.path.join(utility.MSSQLSCRIPTER_DIST_DIRECTORY, pkg)
-            print('Uploading package {}'.format(pkg_path))
-            upload_package(blob_service, pkg_path, 'mssql-scripter')
-    
-        for pkg in os.listdir(utility.MSSQLTOOLSSERVICE_DIST_DIRECTORY):
-            pkg_path = os.path.join(utility.MSSQLTOOLSSERVICE_DIST_DIRECTORY, pkg)
-            pkg_name = os.path.basename(pkg_path).split('-')[0].replace('_', '-').lower()
-            print('Uploading package {}'.format(pkg_name))
-            upload_package(blob_service, pkg_path, pkg_name)
+def publish_daily(platforms_names):
+    """
+    Publish mssql-scripter wheel package to daily storage account.
+    """
+    print('Publishing to simple container within storage account.')
+    assert AZURE_STORAGE_CONNECTION_STRING, 'Set AZURE_STORAGE_CONNECTION_STRING environment variable'
 
-        # Upload the final index file
-        upload_index_file(blob_service, 'index.html', 'Simple Index', UPLOADED_PACKAGE_LINKS)
+    blob_service = BlockBlobService(connection_string=AZURE_STORAGE_CONNECTION_STRING)
+
+    print_heading('Uploading packages to blob storage ')
+    for pkg in os.listdir(utility.MSSQLSCRIPTER_DIST_DIRECTORY):
+        pkg_path = os.path.join(utility.MSSQLSCRIPTER_DIST_DIRECTORY, pkg)
+        print('Uploading package {}'.format(pkg_path))
+        _upload_package(blob_service, pkg_path, 'mssql-scripter')
+        
+    # Upload index files
+    _gen_pkg_index_html(blob_service, 'mssql-scripter')
+    _upload_index_file(blob_service, 'index.html', 'Simple Index', UPLOADED_PACKAGE_LINKS)
+
+
+def publish_official(platforms_names):
+    """
+    Publish mssql-scripter wheel package to PyPi.
+    """
+    mssqlscripter_wheel_dir = os.listdir(utility.MSSQLSCRIPTER_DIST_DIRECTORY)
+    # Run twine action for mssqlscripter.
+    # Only authorized users with credentials will be able to upload this package.
+    # Credentials will be stored in a .pypirc file.
+    for mssqlscripter_wheel_name in mssqlscripter_wheel_dir:
+        utility.exec_command(
+            'twine upload {} pypi'.format(mssqlscripter_wheel_name),
+            utility.MSSQLSCRIPTER_DIST_DIRECTORY)
 
 
 if __name__ == '__main__':
-    build(sys.argv[1:])
+    action = 'build'
+    supported_platforms = [
+        'win32',
+        'win_amd64',
+        'win64',
+        'macosx_10_11_intel',
+        'manylinux1_x86_64',
+        'manylinux1_i686']
+
+    targets = {
+        'build': build,
+        'validate_package': validate_package,
+        'publish_daily': publish_daily,
+        'publish_official': publish_official
+    }
+
+    if len(sys.argv) > 1:
+        action = sys.argv[1]
+    
+    if len(sys.argv) > 2:
+        supported_platforms = [sys.argv[2]]
+    
+    if action in targets:
+        targets[action](supported_platforms)
+    else:
+        print('{} is not a supported action'.format(action))
+        print('Supported actions are {}'.format(list(targets.keys())))
--- utility.py
+++ utility.py
@@ -6,19 +6,14 @@
 from __future__ import print_function
 from subprocess import check_call, CalledProcessError
 import os
+import platform
 import shutil
 import sys
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
-MSSQLTOOLSSERVICE_DIRECTORY = os.path.abspath(os.path.join(
-    os.path.abspath(__file__), '..', 'mssqltoolsservice'))
-
 MSSQLSCRIPTER_DIST_DIRECTORY = os.path.abspath(
     os.path.join(os.path.abspath(__file__), '..', 'dist'))
-
-MSSQLTOOLSSERVICE_DIST_DIRECTORY = os.path.abspath(os.path.join(
-    os.path.abspath(__file__), '..', 'mssqltoolsservice', 'dist'))
 
 
 def exec_command(command, directory, continue_on_error=True):
@@ -51,3 +46,24 @@ def clean_up(directory):
     except Exception:
         # Ignored, directory may not exist which is fine.
         pass
+
+
+def get_current_platform():
+    """
+        Get current platform name.
+    """
+    system = platform.system()
+    arch = platform.architecture()[0]
+
+    run_time_id = None
+    if system == 'Windows':
+        if arch == '32bit':
+            run_time_id = 'win32'
+        elif arch == '64bit':
+            run_time_id = 'win_amd64'
+    elif system == 'Darwin':
+        run_time_id = 'macosx_10_11_intel'
+    elif system == 'Linux':
+        run_time_id = 'manylinux1_x86_64'
+
+    return run_time_id
